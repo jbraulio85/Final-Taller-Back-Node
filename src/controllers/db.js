557d44fbd16a47e04460bee7216c624674a935ee
jbraulio85 <braulioecheverria@kinal.org.gt>
2023-10-08 20:28:29 -0600

    cambiando db a clever cloud

--- src/controllers/db.js
+++ src/controllers/db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2');
 
-/*const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: 'busuhwwiumur94gftjs8-mysql.services.clever-cloud.com',
   user: 'umuacm1goyygasia',
   password: '6A2uNPjpyhM242V7iQRs',
   database: 'busuhwwiumur94gftjs8',
-});*/
+});
 
 
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
   host: 'dbdev.kinal.edu.gt',
   user: 'diegobarrascout',
   password: 'y%XNvghnp3;P2Zy$Us~4',
   database: 'finalTaller',
-});
+});*/
 
 db.connect((err) => {
   if (err) {
